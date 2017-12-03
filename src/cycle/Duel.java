c9918e5d9dbacf0ec7bb82e1971d7fab56735309
TeodorKolev <teodor.kolev@silicon-nile.com>
2017-12-03 22:22:09 +0200

    repeating code problem solved

--- src/cycle/Duel.java
+++ src/cycle/Duel.java
@@ -10,22 +10,22 @@ import java.util.Random;
 
 public class Duel {
 
-    private Player hero;
+    private Player player;
     private Monster monster;
 
     private int round;
 
-    public Duel(Player hero, Monster monster) {
-        this.hero = hero;
+    public Duel(Player player, Monster monster) {
+        this.player = player;
         this.monster = monster;
     }
 
-    public Player getHero() {
-        return hero;
+    public Player getPlayer() {
+        return player;
     }
 
-    public void setHero(Player hero) {
-        this.hero = hero;
+    public void setPlayer(Player player) {
+        this.player = player;
     }
 
     public Monster getMonster() {
@@ -46,27 +46,35 @@ public class Duel {
 
     public void start() {
         Printer.startLogo();
-        hero.attack();
-        monster.defense(hero.getCreature().getDamageDealt(), hero.getCreature().getDamageDealtType());
-        if (iCastable.class.isAssignableFrom(hero.getCreature().getClass())) {
-            Random r = new Random();
-            int randomInt = r.nextInt(100) + 1;
-            if (randomInt <= hero.getSpecialPowerCastChance()) {
-                hero.castSpecial();
-                if (hero.getSpecialPower().getTarget().equals(Constants.TARGET_ENEMY)) {
-                    monster.defense(hero.getCreature().getDamageDealt(), hero.getCreature().getDamageDealtType());
-                }
-            }
+        player.attack();
+        monster.defense(player.getCreature().getDamageDealt(), player.getCreature().getDamageDealtType());
+        if (iCastable.class.isAssignableFrom(player.getCreature().getClass())) {
+            this.castSpecial(player, monster, Boolean.TRUE);
         }
         monster.attack();
-        hero.defense(monster.getCreature().getDamageDealt(), monster.getCreature().getDamageDealtType());
+        player.defense(monster.getCreature().getDamageDealt(), monster.getCreature().getDamageDealtType());
         if (iCastable.class.isAssignableFrom(monster.getCreature().getClass())) {
-            Random r = new Random();
-            int randomInt = r.nextInt(100) + 1;
-            if (randomInt <= monster.getSpecialPowerCastChance()) {
-                monster.castSpecial();
-                if (monster.getSpecialPower().getTarget().equals(Constants.TARGET_ENEMY)) {
-                    hero.defense(monster.getCreature().getDamageDealt(), monster.getCreature().getDamageDealtType());
+            this.castSpecial(player, monster, Boolean.FALSE);
+        }
+    }
+
+    private void castSpecial(Player playerCreature, Monster monsterCreature, boolean heroCast) {
+        Random r = new Random();
+        int randomInt = r.nextInt(100) + 1;
+        if (heroCast) {
+            if (randomInt <= playerCreature.getSpecialPowerCastChance()) {
+                playerCreature.castSpecial();
+                if (playerCreature.getSpecialPower().getTarget().equals(Constants.TARGET_ENEMY)) {
+                    monsterCreature.defense(playerCreature.getCreature().getDamageDealt(),
+                            playerCreature.getCreature().getDamageDealtType());
+                }
+            }
+        } else {
+            if (randomInt <= monsterCreature.getSpecialPowerCastChance()) {
+                monsterCreature.castSpecial();
+                if (monsterCreature.getSpecialPower().getTarget().equals(Constants.TARGET_ENEMY)) {
+                    playerCreature.defense(monsterCreature.getCreature().getDamageDealt(),
+                            monsterCreature.getCreature().getDamageDealtType());
                 }
             }
         }
