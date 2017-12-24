a86f79c4f6508c59c137c4c64034515608233430
TeodorKolev <teodor.kolev@silicon-nile.com>
2017-12-24 19:13:23 +0200

    cleave javadoc

--- src/main/java/com/duels/dungeon/models/spell/spells/Cleave.java
+++ src/main/java/com/duels/dungeon/models/spell/spells/Cleave.java
@@ -3,15 +3,32 @@ package com.duels.dungeon.models.spell.spells;
 import com.duels.dungeon.utils.Constants;
 import com.duels.dungeon.models.spell.SpecialPower;
 
+/**
+ * Represents an cleave.
+ * @author Teodor Kolev
+ * @author https://github.com/TeodorKolev
+ * @version 1.0
+ * @since 1.0
+ */
 public class Cleave extends SpecialPower {
 
     private int damage;
 
+    /**
+     * Creates an cleave with the specified parameter damage.
+     * @param damage The spell power's damage power. Type int.
+     *               Use parameters spell power name (type String),
+     *               spell power damage (type String).
+     */
     public Cleave(int damage) {
         super(Constants.CLEAVE, Constants.DAMAGE_TYPE_MAGIC);
         this.damage = damage;
     }
 
+    /**
+     * Retrieve cleave's damage.
+     * @return cleave's damage. Type int.
+     */
     @Override
     public int getDamage() {
         return damage;
