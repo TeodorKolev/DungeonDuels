34e99a62d7a7e9774ecd8567de42eaf7f03739a9
TeodorKolev <teodor.kolev@silicon-nile.com>
2017-12-26 22:25:14 +0200

    name selector javadoc

--- src/main/java/com/duels/dungeon/cycle/NameSelector.java
+++ src/main/java/com/duels/dungeon/cycle/NameSelector.java
@@ -4,17 +4,34 @@ import com.duels.dungeon.utils.Printer;
 
 import java.util.Scanner;
 
+/**
+ * Represents an name selector.
+ * @author Teodor Kolev
+ * @author https://github.com/TeodorKolev
+ * @version 1.0
+ * @since 1.0
+ */
 public class NameSelector {
 
     private Scanner scanner;
 
+    /**
+     * Creates an name selector.
+     * Defines scanner. Type Scanner class.
+     */
     public NameSelector() {
         this.scanner = new Scanner(System.in);
     }
 
+    /**
+     * Sets player name from user input and capitalize it.
+     * Defines scanner. Type Scanner class.
+     * @return player name. Type String.
+     */
     public String setPlayerName() {
         Printer.chooseName();
         String name = scanner.next();
+        // Capitalize first letter.
         return name.substring(0, 1).toUpperCase() + name.substring(1);
     }
 }
