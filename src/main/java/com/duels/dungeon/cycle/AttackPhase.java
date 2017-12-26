f0f3fe7ceca5b81ec939479c603792c65a6da47e
TeodorKolev <teodor.kolev@silicon-nile.com>
2017-12-26 14:12:35 +0200

    attack phase javadoc

--- src/main/java/com/duels/dungeon/cycle/AttackPhase.java
+++ src/main/java/com/duels/dungeon/cycle/AttackPhase.java
@@ -5,15 +5,33 @@ import com.duels.dungeon.models.creature.monster.Monster;
 import com.duels.dungeon.models.creature.player.Player;
 
 
+/**
+ * Represents an duel attack phase.
+ * @author Teodor Kolev
+ * @author https://github.com/TeodorKolev
+ * @version 1.0
+ * @since 1.0
+ */
 public class AttackPhase {
 
     private RollingDie rollingDie;
     private int randomInt;
 
+    /**
+     * Creates an attack phase.
+     * Defines rolling die. Type RollingDie class.
+     */
     public AttackPhase() {
          this.rollingDie = new RollingDie();
     }
 
+    /**
+     * Attack action. Set player or monster creature to attack distinguished by
+     * heroCast boolean. Adds empowering attack according to random int.
+     * @param player player object parameter. Type Player class.
+     * @param monster monster object parameter. Type Monster class.
+     * @param heroCast player parameter. Type boolean.
+     */
     public void attack(Player player, Monster monster, boolean heroCast) {
         randomInt = rollingDie.roll();
         if (heroCast) {
