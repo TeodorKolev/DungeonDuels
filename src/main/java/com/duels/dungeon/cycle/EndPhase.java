16da4358782574c112937f8c960b6f32f53b7b18
TeodorKolev <teodor.kolev@silicon-nile.com>
2017-12-26 22:17:58 +0200

    end phase javadoc

--- src/main/java/com/duels/dungeon/cycle/EndPhase.java
+++ src/main/java/com/duels/dungeon/cycle/EndPhase.java
@@ -6,18 +6,39 @@ import com.duels.dungeon.utils.Printer;
 
 import java.util.ArrayList;
 
+/**
+ * Represents an duel end phase.
+ * @author Teodor Kolev
+ * @author https://github.com/TeodorKolev
+ * @version 1.0
+ * @since 1.0
+ */
 public class EndPhase {
 
     private Battle battle;
 
+    /**
+     * Creates an end phase.
+     * Defines battle. Type Battle class.
+     */
     public EndPhase(Battle battle) {
         this.battle = battle;
     }
 
+    /**
+     * Prints monster victory.
+     */
     public void monsterDefeatPlayer() {
         Printer.monsterDefeatPlayer();
     }
 
+    /**
+     * Prints player victory. Calculates does monster is last from monsters array
+     * and print end if it is.
+     * @param player player object. Type Player class.
+     * @param round current round. Type int.
+     * @param  monsters array of monsters. Type ArrayList from Monster object.
+     */
     public void playerDefeatMonster(Player player, Monster monster, int round, ArrayList<Monster> monsters) {
         Printer.playerDefeatMonster(player.getName(), monster.getName());
         battle.getDuel().setRound(round += 1);
