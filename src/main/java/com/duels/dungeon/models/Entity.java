57f1bd3f62e4debe41d1fdfca6e217b87fdb8f0b
TeodorKolev <teodor.kolev@silicon-nile.com>
2017-12-26 23:33:38 +0200

    javadoc finished

--- src/main/java/com/duels/dungeon/models/Entity.java
+++ src/main/java/com/duels/dungeon/models/Entity.java
@@ -1,13 +1,28 @@
 package com.duels.dungeon.models;
 
+/**
+ * Represents an entity.
+ * @author Teodor Kolev
+ * @author https://github.com/TeodorKolev
+ * @version 1.0
+ * @since 1.0
+ */
 public class Entity {
 
     private String name;
 
+    /**
+     * Creates entity.
+     * @param name object. Type String.
+     */
     public Entity(String name) {
         this.name = name;
     }
 
+    /**
+     * Retrieve entity's name.
+     * @return current entity's name. Type String.
+     */
     public String getName() {
         return name;
     }
